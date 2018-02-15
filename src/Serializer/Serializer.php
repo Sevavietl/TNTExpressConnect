a3de3fe94b83678b4822903f79a8cbad7c63b491
sevavietl <sevavietl@gmail.com>
2018-02-15 17:18:16 +0200

    Disabled XML serialization formatting.

--- src/Serializer/Serializer.php
+++ src/Serializer/Serializer.php
@@ -3,10 +3,14 @@ namespace TNTExpressConnect\Serializer;
 
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\SerializerBuilder;
+use JMS\Serializer\Handler\HandlerRegistryInterface;
+use JMS\Serializer\Naming\CamelCaseNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
+use JMS\Serializer\Accessor\DefaultAccessorStrategy;
+use JMS\Serializer\XmlSerializationVisitor;
+use JMS\Serializer\XmlDeserializationVisitor;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\DeserializationContext;
-use JMS\Serializer\Handler\HandlerRegistryInterface;
-use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 
 use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
 use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
@@ -22,20 +26,59 @@ class Serializer implements SerializerInterface
     
     public function __construct()
     {
-        $serializerBuilder = SerializerBuilder::create();
+        $this->createSerializerBuilder();
 
+        $this->serializer = $this->serializerBuilder->build();
+    }
+
+    private $serializerBuilder;
+    private function createSerializerBuilder()
+    {
+        $this->serializerBuilder = SerializerBuilder::create();
+
+        $this->addMetadataDirs()
+            ->configureHandlers()
+            ->setVisitors();
+
+        return $this;
+    }
+
+    private function addMetadataDirs()
+    {
         foreach (self::SERVICES as $service) {
-            $serializerBuilder->addMetadataDir(__DIR__ . "/../../metadata/$service", "TNTExpressConnect\\$service");
+            $this->serializerBuilder->addMetadataDir(
+                __DIR__ . "/../../metadata/$service",
+                "TNTExpressConnect\\$service"
+            );
         }
-        
-        $serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) use ($serializerBuilder) {
-            $serializerBuilder->addDefaultHandlers();
+
+        return $this;
+    }
+
+    private function configureHandlers()
+    {
+        $this->serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) {
+            $this->serializerBuilder->addDefaultHandlers();
             $handler->registerSubscribingHandler(new BaseTypesHandler());
             $handler->registerSubscribingHandler(new XmlSchemaDateHandler());
             $handler->registerSubscribingHandler(new AnyTypeHandler());
         });
 
-        $this->serializer = $serializerBuilder->build();
+        return $this;
+    }
+
+    private function setVisitors()
+    {
+        $propertyNamingStrategy = new SerializedNameAnnotationStrategy(new CamelCaseNamingStrategy());
+        $accessorStrategy = new DefaultAccessorStrategy();
+        $serializationVisitor = new XmlSerializationVisitor($propertyNamingStrategy, $accessorStrategy);
+        $serializationVisitor->setFormatOutput(false);
+        $deserializationVisitor = new XmlDeserializationVisitor($propertyNamingStrategy);
+
+        $this->serializerBuilder->setSerializationVisitor('xml', $serializationVisitor);
+        $this->serializerBuilder->setDeserializationVisitor('xml', $deserializationVisitor);
+
+        return $this;
     }
 
     public function serialize($data, $format = 'xml', SerializationContext $context = null)
